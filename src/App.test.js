5919aeaa3b014715210acf2bdd5d09a43d6e7c76
Darren Tebo <darrentebo83@outlook.com>
2020-07-15 23:56:47 -0400

    updated readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("renders ContactForm without crashing", () => {
+  const { getByTestId } = render(<ContactForm />);
+  const form = getByTestId("contact-form");
+  expect(form).toBeInTheDocument();
+});
+
+test("properly validates First Name field", () => {
+  const { getByText } = render(<ContactForm />);
+  const firstName = getByText(/First Name*/i);
+  expect(firstName).toHaveValue('Darren');
 });
